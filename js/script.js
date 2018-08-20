8aa11d80ef6ae43400fc0bb603a546fd810a7346
craigz <craigz+airbook@gmail.com>
2018-08-19 23:29:20 -0400

    index.html was getting out of control with the % of javascript to html
    broke out the script into a separate page (script.js) in a new folder (js) & changed script tag to reference external javascript file.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var car = {
+  make: 'vw',
+  type: 'polo',
+  color: 'blue',
+  isTurnedOn: false,
+  numberOfWheels: 4,
+  seats: [
+    'seat1',
+    'seat2',
+    'seat3',
+    'seat4'
+  ],
+  turnOn: function () {
+    this.isTurnedOn = true;
+  },
+  fly: function () {
+    alert('oh, my fuck! it\'s flying');
+  },
+  switchCar: function (isOn) {
+    console.log('turn car '+isOn);
+    if (isOn == true) {
+      this.isTurnedOn = true;
+    } else {
+      this.isTurnedOn = false;
+    }
+  }
+};
+console.log('the slug was here.');
